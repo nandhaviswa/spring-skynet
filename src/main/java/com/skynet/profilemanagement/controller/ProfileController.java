6afd58faa2e97d8c1d2726ec8970e63d936db3be
Nandha Kumar Viswanathan <nandhakumarviswanaathan@gmail.com>
2022-11-04 22:03:51 +0530

    wire profile service

--- src/main/java/com/skynet/profilemanagement/controller/ProfileController.java
+++ src/main/java/com/skynet/profilemanagement/controller/ProfileController.java
@@ -1,5 +1,11 @@
 package com.skynet.profilemanagement.controller;
 
+import java.util.List;
+
+import com.skynet.profilemanagement.model.Profile;
+import com.skynet.profilemanagement.service.ProfileService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ProfileController {
 
+    @Autowired
+    private ProfileService profileService;
+
     @GetMapping("/getProfileDetails")
-    public String getProfileDetails(@RequestParam(name="ProfileId", required = false) String profileId){        
-        return "Hello World!";
+    public List<Profile> getProfileDetails(@RequestParam(name="ProfileId", required = false) String profileId){
+        List<Profile> profileList = profileService.findAllById();
+        return profileList;
     }
     
 }
